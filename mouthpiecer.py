3056bb2b32b8fe7f77bbb0d6b2fd4ba50e0156da
aleksozolins <aleks@aleksozolins.com>
2022-08-24 16:19:51 -0400

    More login updates, etc.
    - Do not allow operations when not logged in
    - Login indicator
    - Move banner within mainmenu function

--- mouthpiecer.py
+++ mouthpiecer.py
@@ -4,10 +4,15 @@ import requests        # for communicating via API
 import json            # for handling JSON
 import getpass         # provides a password input without revealing text
 
+# declare some vars
+token = ""
+bannerfile = open('banner.txt', 'r')
+banner = bannerfile.read()
 
 # Login process
 def login():
-    print()
+    global token
+    global logemail
     print("Please log in...")
     print()
     logemail = input("Email: ")
@@ -18,17 +23,38 @@ def login():
     response = requests.post(api_url, data=json.dumps(creds), headers=headers)
     jresponse = response.json()
     token = jresponse['session']['user']['token']
-    print("Token: " + token)
+    print()
+    input("You have been logged in. Press Enter...")
+    os.system('clear')
+
+
+# Logout process
+def logout():
+    global token
+    if token == "":
+        input("You aren't currently logged in. Press Enter...")
+        os.system('clear')
+    else:
+        input("You will be logged out. Press Enter...")
+        token = ""
+        os.system('clear')
 
 
 # Our Main Menu
 def mainmenu():
-    print()
+    if token == "":
+        print()
+        print("You are not currently logged in...")
+    else:
+        print()
+        print("You are logged in as " + logemail)
+    print(banner)
     print("[1] Add a mouthpiece")
     print("[2] List my mouthpieces")
     print("[3] Delete a mouthpiece")
     print("-----------------------")
-    print("[7] Log in")
+    print("[6] Log in")
+    print("[7] Log out")
     print("[8] Add a user")
     print("[9] Retrieve a user by ID")
     print("-----------------------")
@@ -61,64 +87,80 @@ def mpcfinishmenu():
 
 # Add mouthpiece process
 def addmpc():
-    newmfr = str(input("Manufacturer: "))
-    print()
-    newmodel = str(input("Model: "))
-    mpctypemenu()
-    newtype = str(input("Mouthpiece type: "))
-    mpcfinishmenu()
-    newfinish = str(input("Finish: "))
-    print()
-    input("Press Enter to send to Knack...")
-    api_url = "https://api.knack.com/v1/objects/object_4/records"
-    mouthpiece = {"field_17": newmfr, "field_24": newtype, "field_16": newmodel, "field_26": newfinish}
-    headers = {"content-type":"application/json", "X-Knack-Application-Id":"60241522a16be4001b611249", "X-Knack-REST-API-KEY":"82d8170b-0661-4462-8dbb-3a589abdfc39"}
-    response = requests.post(api_url, data=json.dumps(mouthpiece), headers=headers)
-    print(response.json())
-    print(response.status_code)
+    if token == "":
+        input("Please log in first. Press Enter...")
+        os.system('clear')
+    else:
+        newmfr = str(input("Manufacturer: "))
+        print()
+        newmodel = str(input("Model: "))
+        mpctypemenu()
+        newtype = str(input("Mouthpiece type: "))
+        mpcfinishmenu()
+        newfinish = str(input("Finish: "))
+        print()
+        input("Press Enter to send to Knack...")
+        api_url = "https://api.knack.com/v1/objects/object_4/records"
+        mouthpiece = {"field_17": newmfr, "field_24": newtype, "field_16": newmodel, "field_26": newfinish}
+        headers = {"content-type":"application/json", "X-Knack-Application-Id":"60241522a16be4001b611249", "X-Knack-REST-API-KEY":"82d8170b-0661-4462-8dbb-3a589abdfc39"}
+        response = requests.post(api_url, data=json.dumps(mouthpiece), headers=headers)
+        print(response.json())
+        print(response.status_code)
 
 
 # List mouthpieces process
 def listmpcs():
-    api_url = "https://api.knack.com/v1/objects/object_4/records"
-    headers = {"content-type":"application/json", "X-Knack-Application-Id":"60241522a16be4001b611249", "X-Knack-REST-API-KEY":"82d8170b-0661-4462-8dbb-3a589abdfc39"}
-    response = requests.get(api_url, headers=headers)
-    jresponse = response.json()
-    print(json.dumps(jresponse, indent=4, sort_keys=True))
+    if token == "":
+        input("Please log in first. Press Enter...")
+        os.system('clear')
+    else:
+        api_url = "https://api.knack.com/v1/objects/object_4/records"
+        headers = {"content-type":"application/json", "X-Knack-Application-Id":"60241522a16be4001b611249", "X-Knack-REST-API-KEY":"82d8170b-0661-4462-8dbb-3a589abdfc39"}
+        response = requests.get(api_url, headers=headers)
+        jresponse = response.json()
+        print(json.dumps(jresponse, indent=4, sort_keys=True))
 
 
 # Delete mouthpiece process
 def delmpc():
-    delid = input("Enter ID of mouthpiece to delete: ")
-    api_url = "https://api.knack.com/v1/objects/object_4/records/" + delid
-    headers = {"content-type":"application/json", "X-Knack-Application-Id":"60241522a16be4001b611249", "X-Knack-REST-API-KEY":"82d8170b-0661-4462-8dbb-3a589abdfc39"}
-    response = requests.delete(api_url, headers=headers)
-    print(response.json())
-    print(response.status_code)
+    if token == "":
+        input("Please log in first. Press Enter...")
+        os.system('clear')
+    else:
+        delid = input("Enter ID of mouthpiece to delete: ")
+        api_url = "https://api.knack.com/v1/objects/object_4/records/" + delid
+        headers = {"content-type":"application/json", "X-Knack-Application-Id":"60241522a16be4001b611249", "X-Knack-REST-API-KEY":"82d8170b-0661-4462-8dbb-3a589abdfc39"}
+        response = requests.delete(api_url, headers=headers)
+        print(response.json())
+        print(response.status_code)
 
 
 # Add user process
 # NOTE: Adding the connected Mouthpiecer in field_40 does not yet work. We may need to retrieve the ID of the new account and then add that value with an additional call.
 def addusr():
-    newusrfname = str(input("First name: "))
-    print()
-    newusrlname = str(input("Last name: "))
-    newusrfullname = newusrfname + "" + newusrlname
-    print()
-    newusremail = str(input("Email: "))
-    print()
-    newusrpasswd1 = getpass.getpass("Password: ")
-    newusrpasswd2 = getpass.getpass("Confirm: ")
-    if newusrpasswd1 != newusrpasswd2:
-        print("Passwords do not match -- Process aborted...")
+    if token != "":
+        input("Please log out before adding a user. Press Enter...")
+        os.system('clear')
     else:
-        input("Press Enter to send to Knack...")
-        api_url = "https://api.knack.com/v1/objects/object_1/records"
-        newusr = {"field_1": {"first": newusrfname, "last": newusrlname}, "field_2": newusremail, "field_3": newusrpasswd1, "field_4": "active", "field_5": "Mouthpiecer"}
-        headers = {"content-type":"application/json", "X-Knack-Application-Id":"60241522a16be4001b611249", "X-Knack-REST-API-KEY":"82d8170b-0661-4462-8dbb-3a589abdfc39"}
-        response = requests.post(api_url, data=json.dumps(newusr), headers=headers)
-        print(response.json())
-        print(response.status_code)
+        newusrfname = str(input("First name: "))
+        print()
+        newusrlname = str(input("Last name: "))
+        newusrfullname = newusrfname + "" + newusrlname
+        print()
+        newusremail = str(input("Email: "))
+        print()
+        newusrpasswd1 = getpass.getpass("Password: ")
+        newusrpasswd2 = getpass.getpass("Confirm: ")
+        if newusrpasswd1 != newusrpasswd2:
+            print("Passwords do not match -- Process aborted...")
+        else:
+            input("Press Enter to send to Knack...")
+            api_url = "https://api.knack.com/v1/objects/object_1/records"
+            newusr = {"field_1": {"first": newusrfname, "last": newusrlname}, "field_2": newusremail, "field_3": newusrpasswd1, "field_4": "active", "field_5": "Mouthpiecer"}
+            headers = {"content-type":"application/json", "X-Knack-Application-Id":"60241522a16be4001b611249", "X-Knack-REST-API-KEY":"82d8170b-0661-4462-8dbb-3a589abdfc39"}
+            response = requests.post(api_url, data=json.dumps(newusr), headers=headers)
+            print(response.json())
+            print(response.status_code)
 
 
 # Retrieve user process
@@ -134,10 +176,7 @@ def rusr():
 
 # Here's where the program runs
 os.system('clear')
-bannerfile = open('banner.txt', 'r')
-banner = bannerfile.read()
-print(banner)
-print("A Python-based command line interface to the Mouthpiecer Knack DB")
+print()
 mainmenu()
 option = int(input("Enter your choice: "))
 
@@ -151,9 +190,12 @@ while option != 0:
     elif option == 3:
         print()
         delmpc()
-    elif option == 7:
+    elif option == 6:
         print()
         login()
+    elif option == 7:
+        print()
+        logout()
     elif option == 8:
         print()
         addusr()
@@ -168,5 +210,6 @@ while option != 0:
     mainmenu()
     option = int(input("Enter your choice: "))
 
+bannerfile.close()
 print()
 print("You've exited to shell")
