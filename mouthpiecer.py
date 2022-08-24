7acc7a6525550af699c4da39635084299b23fe46
aleksozolins <aleks@aleksozolins.com>
2022-08-23 23:29:28 -0400

    New login process + adding a user process added.

--- mouthpiecer.py
+++ mouthpiecer.py
@@ -27,6 +27,11 @@ def mainmenu():
     print("[1] Add a mouthpiece")
     print("[2] List my mouthpieces")
     print("[3] Delete a mouthpiece")
+    print("-----------------------")
+    print("[7] Log in")
+    print("[8] Add a user")
+    print("[9] Retrieve a user by ID")
+    print("-----------------------")
     print("[0] Exit to shell")
     print()
 
@@ -92,13 +97,47 @@ def delmpc():
     print(response.status_code)
 
 
+# Add user process
+# NOTE: Adding the connected Mouthpiecer in field_40 does not yet work. We may need to retrieve the ID of the new account and then add that value with an additional call.
+def addusr():
+    newusrfname = str(input("First name: "))
+    print()
+    newusrlname = str(input("Last name: "))
+    newusrfullname = newusrfname + "" + newusrlname
+    print()
+    newusremail = str(input("Email: "))
+    print()
+    newusrpasswd1 = getpass.getpass("Password: ")
+    newusrpasswd2 = getpass.getpass("Confirm: ")
+    if newusrpasswd1 != newusrpasswd2:
+        print("Passwords do not match -- Process aborted...")
+    else:
+        input("Press Enter to send to Knack...")
+        api_url = "https://api.knack.com/v1/objects/object_1/records"
+        newusr = {"field_1": {"first": newusrfname, "last": newusrlname}, "field_2": newusremail, "field_3": newusrpasswd1, "field_4": "active", "field_5": "Mouthpiecer"}
+        headers = {"content-type":"application/json", "X-Knack-Application-Id":"60241522a16be4001b611249", "X-Knack-REST-API-KEY":"82d8170b-0661-4462-8dbb-3a589abdfc39"}
+        response = requests.post(api_url, data=json.dumps(newusr), headers=headers)
+        print(response.json())
+        print(response.status_code)
+
+
+# Retrieve user process
+def rusr():
+    usrid = (input("Enter user ID:"))
+    input("Press Enter to send to Knack...")
+    api_url = "https://api.knack.com/v1/objects/object_1/records/" + usrid
+    headers = {"content-type":"application/json", "X-Knack-Application-Id":"60241522a16be4001b611249", "X-Knack-REST-API-KEY":"82d8170b-0661-4462-8dbb-3a589abdfc39"}
+    response = requests.get(api_url, headers=headers)
+    print(response.json())
+    print(response.status_code)
+
+
 # Here's where the program runs
 os.system('clear')
 bannerfile = open('banner.txt', 'r')
 banner = bannerfile.read()
 print(banner)
 print("A Python-based command line interface to the Mouthpiecer Knack DB")
-login()
 mainmenu()
 option = int(input("Enter your choice: "))
 
@@ -112,6 +151,15 @@ while option != 0:
     elif option == 3:
         print()
         delmpc()
+    elif option == 7:
+        print()
+        login()
+    elif option == 8:
+        print()
+        addusr()
+    elif option == 9:
+        print()
+        rusr()
     else:
         print()
         print("Invalid option selected")
